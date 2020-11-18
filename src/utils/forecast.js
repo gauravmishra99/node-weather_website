e98a81004a295f981829e87f2d1397d8f14cccd3
gauravmishra99 <gauravmishra.190699@gmail.com>
2020-11-18 23:55:14 +0530

    changes made to show addtional data shown in the weather search

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -21,7 +21,7 @@ const forecast = (longitude, latitude, callback) => {
           body.current.temperature +
           " degress out. It feels like " +
           body.current.feelslike +
-          " degress out."
+          " degress out." + " The visibility is " + body.current.visibility
       );
     }
   });
