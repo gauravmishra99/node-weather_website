b02f9752e0c4b4823d90d31924bf46d4c53b6720
gauravmishra99 <gauravmishra.190699@gmail.com>
2020-11-18 21:09:48 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -62,33 +63,36 @@ app.get("/help", (req, res) => {
 // });
 
 app.get("/weather", (req, res) => {
-  if (!req.query.address){
+  if (!req.query.address) {
     return res.send({
-      error: "You must provide an address"
-    })
+      error: "You must provide an address",
+    });
   }
 
-  geocode(req.query.address, (error, {latitude, longitude, location}= {})=>{
-    if (error){
-      return res.send({
-        error
-      })
-    }
-
-    forecast(longitude, latitude, (error, forecastData)=>{
-      if (error){
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
         return res.send({
-          error
-        })
+          error,
+        });
       }
 
-      res.send({
-        forecast: forecastData,
-        location,
-        address: req.query.address
-      })
-    })
-  })
+      forecast(longitude, latitude, (error, forecastData) => {
+        if (error) {
+          return res.send({
+            error,
+          });
+        }
+
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        });
+      });
+    }
+  );
 });
 
 app.get("/products", (req, res) => {
@@ -119,6 +123,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000.");
+app.listen(port, () => {
+  console.log("Server is up on port " + port + '.');
 });
